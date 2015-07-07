0ae997ef6b689416de2709bdf18b5f24fc28b9ec
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-07 14:21:36 -0700

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -11,12 +11,35 @@ private:
 	
 public:
 	linked_list();
+	void append( int);
 
 };
 linked_list :: linked_list()
 {
 	Head = 0;
 }
+void linked_list:: append( int num)
+{		
+	node * newNode = new node;
+	newNode->value = num;
+	
+	if (Head)
+	{
+		node * one = Head -> Next;
+		while (one)
+		{
+			one = one->Next;
+		}
+
+		one->Next = newNode;
+	}
+	else
+	{
+		Head = newNode;
+	}
+	
+}
+
 int main ()
 {
 	return 0;
