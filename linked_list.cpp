3f01f61169325f862d3e736571d8ed5caa9e85a7
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-07-08 11:34:36 -0700

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,46 +1,81 @@
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "List.h"
+
+
 #include <iostream>
-class linked_list
+using namespace std;
+template <class T>
+class LinkedList : public List
 {
 private:
-	struct node
-	{
-		int value;
-		node * Next;
-	};
-	node * Head;
-	
+    struct node
+    {
+        T value;
+        node * Next;
+    };
+    node * Head;
+
 public:
-	linked_list();
-	void append( int);
-
+        void append (T);
+        void remove (T);
 };
-linked_list :: linked_list()
+template <class T>
+void LinkedList <T>::append (T data)
 {
-	Head = 0;
-}
-void linked_list:: append( int num)
-{		
-	node * newNode = new node;
-	newNode->value = num;
-	
-	if (Head)
-	{
-		node * one = Head -> Next;
-		while (one)
-		{
-			one = one->Next;
-		}
+    node * newNode = new node;
+    newNode->value = data;
+    if (Head)
+    {
+        node * one = Head -> Next;
+        while (one)
+        {
+            one = one->Next;
+        }
 
-		one->Next = newNode;
-	}
-	else
-	{
-		Head = newNode;
-	}
-	
+        one->Next = newNode;
+    }
+    else
+    {
+        Head = newNode;
+    }
 }
 
-int main ()
+template <class T>
+void LinkedList <T>::remove (T data)
 {
-	return 0;
+        node * one = Head;
+    if (!Head)
+    {
+        return;
+    }
+
+    else if (Head->value == data)
+    {
+        Head = Head->Next;
+        delete one;
+
+    }
+    else
+    {
+        node * two = one->next;
+        while (two->value != data && two->Next != NULL)
+        {
+            one = one->Next;
+            two = two->Next;
+        }
+        if (two)
+        {
+            one->Next = two->Next;
+            delete two;
+
+        }
+        return;
+    }
+
 }
+
+
+#endif
