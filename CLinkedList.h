45ffe0ca9b0fa96b5328dc1e54800dd460d22944
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-09 14:27:02 -0700

    Create CLinkedList.h
    Implementation of a circularly linked List.

---
+++ CLinkedList.h
@@ -0,0 +1,235 @@
+#ifndef CLINKEDLIST_H
+#define CLINKEDLIST_H
+#include "List.h"
+#include <iostream>
+using namespace std;
+
+class CLinkedList : List
+{
+    struct Node
+    {
+        int data;
+        Node * prev;    //need to delete this
+        Node * next;
+    };
+    Node * Head;
+    Node * current;
+    Node * Last;
+
+    public:
+    CLinkedList ()
+    {
+        Last = current = Head = NULL;
+    }
+    CLinkedList ( const CLinkedList & f)
+    {
+        *this = f;
+    }
+    ~CLinkedList()
+    {
+        clear();
+    }
+    void clear();
+    bool isEmpty() const;
+    bool isFull() const;
+    int insert (int);
+    void print () const;
+    int getLength() const;
+    void reset();
+    int getNext (int &);
+    bool find (int) const;
+    int remove (int);
+    void rprint() const;
+    CLinkedList & operator = (const CLinkedList & f)
+    {
+        Node * one  = f.Head;
+        while (one)
+        {
+            insert(one->data);
+            one = one->next;
+        }
+        return *this;
+    }
+    bool operator < (const CLinkedList & f)
+    {
+        int length1 = getLength();
+        int length2 = f.getLength();
+        return length1 < length2;
+
+    }
+    bool operator ==(const CLinkedList & f)
+    {
+        int length1 = getLength();
+        int length2 = f.getLength();
+        return length1 == length2;
+
+    }
+};
+
+
+void CLinkedList::clear()
+{
+    Node * Start = Head;
+    Node * one = NULL;
+    while (Head)
+    {
+        one = Head;
+        Head = Head->next;
+        delete one;
+        if (Head == Start)
+        {
+            current = Last = Head = NULL;
+        }
+    }
+}
+bool CLinkedList::isEmpty() const
+{
+    return Head == NULL;
+}
+
+bool CLinkedList::isFull() const
+{
+    Node * newNode;
+    try
+    {
+        newNode = new Node;
+        delete newNode;
+        return false;
+    }
+    catch (bad_alloc Name)
+    {
+        return true;
+    }
+}
+int CLinkedList::insert(int value)
+{
+    if (isFull())
+        return -1;
+    Node * newNode = new Node;
+    if (!newNode)
+        return -1;
+    newNode->data = value;
+    newNode->next = NULL;
+    if (isEmpty())
+    {
+         Head = newNode;
+         current =  Last = Head;
+        return 0;
+    }
+    if ((Head->data) > value)
+    {
+        newNode->next = Head;
+        Head = newNode;
+        Last->next = Head;
+        return 0;
+    }
+    Node * one = Head;
+    Node * two = Head->next;
+    for (; two !=Head && two->data < value; one = one->next, two = two->next);
+
+    one->next = newNode;
+    newNode->next = two;
+    if (two == Head)
+           Last = newNode;
+
+    return 0;
+}
+
+void CLinkedList::print() const
+{
+    if (isEmpty())
+        return;
+    Node * one = Head;
+    do
+    {
+        cout << one ->data << " " ;
+        one = one->next;
+    } while(one != Head);
+
+}
+
+int CLinkedList::getLength() const
+{
+    if (isEmpty())
+        return 0;
+    Node * one = Head;
+    Node * Start = Head;
+    int length = 0;
+    do
+    {
+        length++;
+        one = one->next;
+    }while(one !=Head);
+    return length;
+}
+
+void CLinkedList::reset()
+{
+    current = Head;
+}
+
+int CLinkedList::getNext (int & value)
+{
+    if (current ==NULL)
+        return -1;
+
+    value = current->data;
+    current = current->next;
+    if (current ==NULL)
+        current = Head;
+    return 0;
+
+}
+
+bool CLinkedList::find(int value) const
+{
+    Node * one = Head;
+    for (; one && one->data != value; one = one->next);
+    if (one)
+        return true;
+    return false;
+}
+
+int CLinkedList::remove(int value)
+{
+    if (isEmpty())
+        return -1;
+    if (Head->data == value)
+    {
+        Node * one = Head;
+        Head = Head->next;
+        if (Head)
+            Head->prev = NULL;
+        delete one;
+        return 0;
+    }
+    Node * one = Head;
+    Node * two = Head->next;
+    for(; two && two->data != value; one = one->next, two = two->next);
+    if (two)
+    {
+        one->next = two->next;
+        if (two->next)
+            two->next->prev = one;
+        delete two;
+        return 0;
+    }
+    else
+        return -1;
+}
+
+void CLinkedList::rprint() const
+{
+    if (isEmpty())
+        return;
+    Node * one = Head;
+    while (one->next)
+        one = one->next;
+    while (one)
+    {
+        cout << one->data << " ";
+        one = one->prev;
+    }
+    cout <<endl;
+}
+#endif
