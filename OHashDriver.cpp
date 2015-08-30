5908578cb3ec1d4b788a3ce9c024cca5075d9a95
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-08-30 13:11:32 -0700

    Update OHashDriver.cpp

--- OHashDriver.cpp
+++ OHashDriver.cpp
@@ -1,1 +1,28 @@
+#include "OHash.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    OHash test;
+    cout << test.isEmpty() <<endl;
+        cout << test.isFull() <<endl;
 
+    test.insert("Hello");
+    test.insert("Jello");
+
+        test.insert("Sello");
+            test.insert("Tello");
+
+    test.insert("Hello");
+    test.insert("World");
+    test.insert("Goodbye");
+    test.print();
+    cout << test.search("Hello") << endl;
+    test.remove("World");
+    test.print();
+
+            test.print();
+            test.clear();
+            cout << test.isEmpty() <<endl;
+    return 0;
+}
