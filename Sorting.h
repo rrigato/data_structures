538252420f7dfa06b171db9907bd89eac1d16cb8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-04 15:49:22 -0700

    Update Sorting.h

--- Sorting.h
+++ Sorting.h
@@ -1,22 +1,35 @@
 #ifndef SORTING_H
 #define SORTING_H
-#endif // SORTING_H
+
 #include <iostream>
 using std:: cout;
 using std:: endl;
-template <class T>
-class sorting 
+template <class Z>
+class Sorting
 {
 public:
-    merge_sort(T, int, int);
-    
+    void merge_sort(Z [], int, int);
+    void merge(Z [], int, int, int, int);
+
 };
-void merge_sort;
-
-    int * array_name = new int [250];
+template <class Z>
+void Sorting<Z>:: merge(Z array_name[], int left_start, int left_end, int right_start, int right_end)
+{
+    if( array_name[left_end] < array_name[right_start])
+        return;
+}
+template <class Z>
+void Sorting<Z> :: merge_sort (Z array_name[], int start, int end)
+{
+    if (start == end)
+        return;
+    int middle = (end +);
+    merge_sort(array_name, start, middle);
+    merge_sort(array_name, middle, end);
     
-    while(i != 250)
-    
-    return 0;
 
+
+
+
+}
 #endif
