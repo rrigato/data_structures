7a2424b01a0c7e72256ca4e82aee34c3fdbe297d
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-04 15:23:17 -0700

    Create Sorting.h
    This class contains some templatized versions of sorting algorithms.

---
+++ Sorting.h
@@ -0,0 +1,22 @@
+#ifndef SORTING_H
+#define SORTING_H
+#endif // SORTING_H
+#include <iostream>
+using std:: cout;
+using std:: endl;
+template <class T>
+class sorting 
+{
+public:
+    merge_sort(T, int, int);
+    
+};
+void merge_sort;
+
+    int * array_name = new int [250];
+    
+    while(i != 250)
+    
+    return 0;
+
+#endif
