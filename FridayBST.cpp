562092a43bcff90cf77e8662f374e4a128e77a98
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-04-01 16:56:51 -0700

    Update FridayBST.cpp
    Added a few basic test cases

--- FridayBST.cpp
+++ FridayBST.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
+    BinaryTree B;
+    B.insert(5);
+    B.insert(2);
+    B.insert(7);
+    B.print();
+    B.destroy();
+    std::cout << std::endl;
+    B.print();
+    std::cin.get();
     return 0;
 }
