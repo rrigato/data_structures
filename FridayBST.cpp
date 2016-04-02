1446dc0e476a7fbefd7eaf0cb798706628b5b2fb
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-04-01 18:24:55 -0700

    Update FridayBST.cpp
    Testing the remove function

--- FridayBST.cpp
+++ FridayBST.cpp
@@ -6,10 +6,23 @@ int main()
     B.insert(5);
     B.insert(2);
     B.insert(7);
+    for(int i = 20; i > 11; i--)
+    {
+        B.insert(i);
+    }
     B.print();
+    for (int z = 0; z <= 20; z++)
+    {
+        B.Remove(z);
+    }
+    B.Remove(5);
+    B.print();
+    std::cout << B.isEmpty() <<std::endl;
     B.destroy();
     std::cout << std::endl;
+    std::cout << B.isEmpty() <<std::endl;
     B.print();
     std::cin.get();
     return 0;
 }
+
